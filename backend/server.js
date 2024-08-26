231cedcdb944d70f752309289bde75cb8dcbb6b7
tesddev <tesleem.amuda@gmail.com>
2024-08-26 23:48:19 +0100

    add new router to server listener

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import config from "./config.js";
 import dbConnection from "./db/dbConnection.js";
 import authRouter from "./routes/auth.js";
+import userRouter from "./routes/userManagement.js";
 import cors from "cors"
 
 const app = express();
@@ -10,7 +11,8 @@ app.use(express.json());
 app.use(cors())
 
 //Routes
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 dbConnection()
 app.listen(config.port, () => {
