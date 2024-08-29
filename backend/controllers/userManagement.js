91d8815be5eb8651df33bfaa1a2ca37e081c4ebb
tesddev <tesleem.amuda@gmail.com>
2024-08-29 12:21:21 +0100

    add  create and edit user end point

--- backend/controllers/userManagement.js
+++ backend/controllers/userManagement.js
@@ -38,7 +38,6 @@ const isAdmin = (req, res, next) => {
     next();
 };
 
-// Route to update the password
 const updatePassword = async (req, res) => {
     try {
         // Ensure the token is authenticated before proceeding
@@ -196,5 +195,130 @@ const getAllUsers = async (req, res) => {
     }
 }
 
+const createUser = async (req, res) => {
+    const registerSchema = Joi.object({
+        fullName: Joi.string().required(),
+        email: Joi.string().email().required(),
+        phoneNumber: Joi.string().required(),
+        username: Joi.string().required(),
+        password: Joi.string().min(8).required(),
+        role: Joi.string().optional(),
+    });
 
-export { authenticateToken, updatePassword, updateProfile, getAllUsersCount, deleteUser, isAdmin, getAllUsers };
+    try {
+        // Ensure the token is authenticated and the user is an admin
+        authenticateToken(req, res, async () => {
+            if (req.user.role !== 'admin') {
+                return res.status(403).json({
+                    responseCode: apiResponseCode.UNAUTHORIZED_ACCESS,
+                    responseMessage: "Access denied, admin privileges required"
+                });
+            }
+
+            // validate user/client request
+            const { error } = registerSchema.validate(req.body);
+            if (error) {
+            return res.status(400).json({
+                responseCode: apiResponseCode.BAD_REQUEST,
+                responseMessage: error.details[0].message,
+                data: null,
+            });
+            }
+
+            const { fullName, email, phoneNumber, username, password, role } = req.body;
+
+            // Check if all required fields are provided
+            if (!fullName || !email || !phoneNumber || !username || !password ) {
+                return res.status(400).json({
+                    responseCode: apiResponseCode.BAD_REQUEST,
+                    responseMessage: "All fields are required"
+                });
+            }
+
+            // Hash the password before saving it
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Create the new user
+            const newUser = new User({
+                fullName,
+                email,
+                phoneNumber,
+                username,
+                password: hashedPassword,
+                role: role || 'user' 
+            });
+
+            // Save the user to the database
+            await newUser.save();
+
+            res.status(201).json({
+                responseCode: apiResponseCode.SUCCESSFUL,
+                responseMessage: "User created successfully",
+                data: {
+                    fullName: newUser.fullName,
+                    username: newUser.username,
+                    email: newUser.email,
+                    phoneNumber: newUser.phoneNumber,
+                    role: newUser.role
+                }
+            });
+        });
+    } catch (error) {
+        res.status(500).json({
+            responseCode: apiResponseCode.INTERNAL_SERVER_ERR,
+            responseMessage: "Internal Server Error",
+            error: error.message
+        });
+    }
+}
+
+const editUser = async (req, res) => {
+    try {
+        // Ensure the token is authenticated before proceeding
+        authenticateToken(req, res, async () => {
+
+            const { _id } = req.params;
+            const { fullName, email, phoneNumber, username, password } = req.body;
+
+            // Find the user by ID
+            const user = await User.findById(_id);
+            if (!user) {
+                return res.status(404).json({
+                    responseCode: apiResponseCode.MISSING_DETAILS,
+                    responseMessage: "User not found"
+                });
+            }
+
+            // Update the fields that are provided in the request body
+            if (fullName) user.fullName = fullName;
+            if (email) user.email = email;
+            if (phoneNumber) user.phoneNumber = phoneNumber;
+            if (username) user.username = username;
+            if (password) {
+                const hashedPassword =  await bcrypt.hash(password, 10);
+                user.password = hashedPassword;
+            }
+            // Save the updated user to the database
+            await user.save();
+
+            res.status(200).json({
+                responseCode: apiResponseCode.SUCCESSFUL,
+                responseMessage: "User profile updated successfully",
+                data: {
+                    fullName: user.fullName,
+                    username: user.username,
+                    email: user.email,
+                    phoneNumber: user.phoneNumber
+                }
+            });
+        });
+    } catch (error) {
+        res.status(500).json({
+            responseCode: apiResponseCode.INTERNAL_SERVER_ERR,
+            responseMessage: "Internal Server Error",
+            error: error.message
+        });
+    }
+}
+
+export { authenticateToken, updatePassword, updateProfile, getAllUsersCount, deleteUser, isAdmin, getAllUsers, createUser, editUser };
