c63ce80d35534db612a72e974bef2ffe5685391c
tesddev <tesleem.amuda@gmail.com>
2024-08-29 12:22:01 +0100

    add additional routes

--- backend/routes/userManagement.js
+++ backend/routes/userManagement.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { authenticateToken, updatePassword, updateProfile } from "../controllers/userManagement.js"
+import { authenticateToken, getAllUsersCount, updatePassword, updateProfile, isAdmin, deleteUser, getAllUsers, editUser, createUser } from "../controllers/userManagement.js"
 
 const userRouter = express.Router();
 
 userRouter.put('/update-password', authenticateToken, updatePassword);
 
 userRouter.put('/update-profile', authenticateToken, updateProfile);
+
+userRouter.get('/get-all-users-count', authenticateToken, getAllUsersCount);
+
+userRouter.delete('/users/:_id', authenticateToken, isAdmin, deleteUser);
+
+userRouter.get('/get-all-users', authenticateToken, isAdmin, getAllUsers);
+
+userRouter.post('/create-user', authenticateToken, isAdmin, createUser);
+
+userRouter.put('/edit-user/:_id', authenticateToken, isAdmin, editUser);
 
 export default userRouter;
