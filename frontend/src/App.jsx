a7f6089f39c77135ae09c0b75075eef59ce75690
DonaShaibu <donertus@gmail.com>
2024-09-02 20:47:36 +0100

    Home button has been added to take you to the products page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Product from "./assets/Product"
 import ProductDetails from "./assets/ProductDetails"
+import Home from "./assets/Home";
 
 
 
@@ -14,6 +15,7 @@ const App = ()=> {
         <Routes>
               <Route path = "/Products" element = {<Product/>} />
               <Route path = "/product" element = {<ProductDetails/>} />
+              <Route path = "/" element = {<Home/>} />
           </Routes>
      
 
