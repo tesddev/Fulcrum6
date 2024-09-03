68b1bbb09a9cf98c13ca486e4145bb6a18215e17
DonaShaibu <donertus@gmail.com>
2024-09-03 14:40:36 +0100

    updated products code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Product from "./assets/Product"
-import ProductDetails from "./assets/ProductDetails"
-import Home from "./assets/Home";
+import Product from "./products/Product"
+import ProductDetails from "./products/ProductDetails"
+import Home from "./products/Home";
 
 
 
@@ -13,8 +13,8 @@ const App = ()=> {
     <div>
      
         <Routes>
-              <Route path = "/Products" element = {<Product/>} />
-              <Route path = "/product" element = {<ProductDetails/>} />
+              <Route path = "/products" element = {<Product/>} />
+              <Route path = "/products/:id" element = {<ProductDetails/>} />
               <Route path = "/" element = {<Home/>} />
           </Routes>
      
