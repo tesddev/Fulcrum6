58fba6d8b246b96268649a0e6247747716f44d91
tesddev <tesleem.amuda@gmail.com>
2024-09-04 15:22:42 +0100

    add more constants to endpoints

--- frontend/src/api/endpoints.js
+++ frontend/src/api/endpoints.js
@@ -2,5 +2,12 @@ export const endpoints = {
     auth: {
         register: "auth/register",
         login: "auth/login"
-    }
+    },
+
+    dashboard: {
+        userCount: "user/get-all-users-count",
+        productCount: "product/get-all-products-count"
+    },
+
+    bearerToken: `Bearer ${sessionStorage.getItem("***")}`
 }
